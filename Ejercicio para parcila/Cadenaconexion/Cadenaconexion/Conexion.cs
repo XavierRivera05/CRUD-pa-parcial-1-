cea791935c0e939bebeb28e27a111ec19960a14c
XavierRivera05 <126819995+XavierRivera05@users.noreply.github.com>
2024-08-30 13:07:49 -0600

    Add files via upload

---
+++ Ejercicio para parcila/Cadenaconexion/Cadenaconexion/Conexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Cadenaconexion
+{
+    internal class Conexion
+    {
+        public static SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection("SERVER=DESKTOP-7IK1942\\SQLEXPRESS;DATABASE=Northwind;integrated security=true;");
+            cn .Open();
+            return cn;
+        }
+
+    }
+}
